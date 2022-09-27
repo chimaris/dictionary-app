97617fc7ca394efb100fd00b35e405e3dc5c569e
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-27 21:59:36 +0100

    add search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<button className="btn btn-primary">Hello World</button>
-			</header>
-		</div>
-	);
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Stella Maris</small>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
